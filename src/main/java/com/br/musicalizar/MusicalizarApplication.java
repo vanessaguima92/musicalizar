1cbf580b8814618c3beaa49aa1bdfc39afe5e4fd
Vanessa <vanessaguima@outlook.com>
2024-01-11 19:43:48 -0300

    iniciando projeto curso

---
+++ src/main/java/com/br/musicalizar/MusicalizarApplication.java
@@ -0,0 +1,13 @@
+package com.br.musicalizar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalizarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalizarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/musicalizar/MusicalizarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.musicalizar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalizarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
